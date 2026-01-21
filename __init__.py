b64de70f263e26c9b57ff9f602e1798e59659d25
Isi <isinsemail@gmail.com>
2026-01-21 10:30:57 +0100

    Comment out unused imports and route setup
    Comment out imports and conditional route addition for PromptServer.

--- __init__.py
+++ __init__.py
@@ -273,16 +273,16 @@ __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
 WEB_DIRECTORY = "./web"
 
-from aiohttp import web
-from server import PromptServer
-from pathlib import Path
+# from aiohttp import web
+# from server import PromptServer
+# from pathlib import Path
 
-if hasattr(PromptServer, "instance"):
-    try:
-        # NOTE: we add an extra static path to avoid comfy mechanism
-        # that loads every script in web.
-        PromptServer.instance.app.add_routes(
-            [web.static("/kjweb_async", (Path(__file__).parent.absolute() / "kjweb_async").as_posix())]
-        )
-    except:
-        pass
+# if hasattr(PromptServer, "instance"):
+#     try:
+#         # NOTE: we add an extra static path to avoid comfy mechanism
+#         # that loads every script in web.
+#         PromptServer.instance.app.add_routes(
+#             [web.static("/kjweb_async", (Path(__file__).parent.absolute() / "kjweb_async").as_posix())]
+#         )
+#     except:
+#         pass
